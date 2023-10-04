0672a15941d39a86208f9e3820cfab990ddc7f02
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-03 23:23:34 -0300

    feat(App): initial structure page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { Trash } from 'phosphor-react'
+
 import styles from './App.module.css'
 import Header from './components/Header'
+import Input from './components/Input'
 
 function App() {
   return (
@@ -7,7 +10,33 @@ function App() {
       <Header/>
       
       <div className={styles.wrapper}>
-        Content
+        <main className={styles.content}>
+          
+        <div className={styles.createTaskContent}>
+          <Input/>
+        </div>
+
+        <div className={styles.tableTitle}>
+          <div>
+            <span>Tarefas criadas</span> <span>5</span>
+          </div>
+
+          <div>
+            <span>Concluídas</span> <span>2 de 5</span>
+          </div>
+        </div>
+
+        <section>
+          <div>
+            <input type="checkbox" name="task" id="task" />
+            Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+            <button type="button">
+              <Trash/>
+            </button>
+          </div>
+        </section>
+
+        </main>
       </div>
     </>
   )
