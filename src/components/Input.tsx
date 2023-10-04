08797d11b37e172ce0b3e8099c02e6948e27f57f
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-04 02:24:37 -0300

    feat(Input): adds input html props

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,16 @@
+import { FC, InputHTMLAttributes } from 'react'
+
 import styles from './Input.module.css'
 
-export default function Input(){
+const Input: FC<InputHTMLAttributes<HTMLInputElement>> = (props) =>{ 
   return (
     <input 
       className={styles.inputWrapper} 
       type="text"
       placeholder='Adicione uma nova tarefa'
+      {...props}
     />
   )
 }
+
+export default Input
